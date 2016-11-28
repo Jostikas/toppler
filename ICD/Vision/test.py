4c03aa0e4721b22b859087834a259b3583d61ab1
Jyrgen <jyrgen1k0@gmail.com>
2016-11-28 21:04:54 +0200

    Hough working, start work on perp transform.

--- ICD/Vision/test.py
+++ ICD/Vision/test.py
@@ -1,7 +1,7 @@
 import cv2
 
-logi = cv2.VideoCapture(0)
-pseye = cv2.VideoCapture(1)
+logi = cv2.VideoCapture(1)
+pseye = cv2.VideoCapture(2)
 
 pseye.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 pseye.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -10,17 +10,17 @@ logi.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
 logi.set(cv2.CAP_PROP_FRAME_HEIGHT, 448)
 logi.set(cv2.CAP_PROP_FPS, 30)
 
-# cv2.namedWindow('LOGITECH', True)
-# cv2.namedWindow('PS3EYE', True)
-# cv2.moveWindow('PSEye', 850, 200)
+cv2.namedWindow('LOGITECH', True)
+cv2.namedWindow('PS3EYE', True)
+cv2.moveWindow('PSEye', 850, 200)
 
 while True:
     logi.grab()
     pseye.grab()
 
     ret, l_frame = logi.retrieve()
-    # ret, p_frame = pseye.retrieve()
+    ret, p_frame = pseye.retrieve()
     cv2.imshow('LOGITECH', l_frame)
-    # cv2.imshow('PS3EYE', p_frame)
-    if cv2.waitKey(1) == -1:
+    cv2.imshow('PS3EYE', p_frame)
+    if cv2.waitKey(1) != -1:
         break
--- ICD/constants.py
+++
@@ -1,3 +0,0 @@
-FRAME_W = 800
-FRAME_H = 448
-FPS = 30
