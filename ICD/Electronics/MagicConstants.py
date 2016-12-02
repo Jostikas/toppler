14a1a9f0acdcf8d463b66bc4ca0da94da335575d
jyrgenlaks <jyrgenlaks@gmail.com>
2016-12-02 22:27:56 +0200

    Create MagicConstants.py

---
+++ ICD/Electronics/MagicConstants.py
@@ -0,0 +1,37 @@
+##################################################
+#   _____            _               _       __  #
+#  |  __ \          | |             | |     /_ | #
+#  | |__) |   ___   | |__     ___   | |_     | | #
+#  |  _  /   / _ \  | '_ \   / _ \  | __|    | | #
+#  | | \ \  | (_) | | |_) | | (_) | | |_     | | #
+#  |_|  \_\  \___/  |_.__/   \___/   \__|    |_| #
+#                                                #
+##################################################
+# ehk siis see, mis on SININE ja arvutist KAUGEMAL
+# ehk siis see, mille non-remapped ID on 1
+
+ROBOT_1_MOVE_FORWARD_LEFT_MOTOR_SPEED = 400
+ROBOT_1_MOVE_FORWARD_RIGHT_MOTOR_SPEED = 300
+
+ROBOT_1_MOVE_BACKWARD_LEFT_MOTOR_SPEED = -10
+ROBOT_1_MOVE_BACKWARD_RIGHT_MOTOR_SPEED = -200
+
+
+
+####################################################
+#   _____            _               _       ___   #
+#  |  __ \          | |             | |     |__ \  #
+#  | |__) |   ___   | |__     ___   | |_       ) | #
+#  |  _  /   / _ \  | '_ \   / _ \  | __|     / /  #
+#  | | \ \  | (_) | | |_) | | (_) | | |_     / /_  #
+#  |_|  \_\  \___/  |_.__/   \___/   \__|   |____| #
+#                                                  #
+####################################################
+# ehk siis see, mis on ORANZ ja arvutile LÄHEMAL
+# ehk siis see, mille non-remapped ID on 0
+ROBOT_2_MOVE_FORWARD_LEFT_MOTOR_SPEED = 800
+ROBOT_2_MOVE_FORWARD_RIGHT_MOTOR_SPEED = 100
+
+ROBOT_2_MOVE_BACKWARD_LEFT_MOTOR_SPEED = -800
+ROBOT_2_MOVE_BACKWARD_RIGHT_MOTOR_SPEED = -100
+
