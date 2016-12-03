5cca7b22ff0ed9b6b731f4ef0617ea7efcc1e348
DESKTOP-9C5E9F3\Myrka <jyrgenlaks@gmail.com>
2016-12-03 20:58:18 +0200

    Added start and stop command callbacks (now it actually works!). Also changed a filename.

--- ICD/Electronics/ICDelectronics_test.py
+++ ICD/Electronics/ICDelectronics_test.py
@@ -2,12 +2,14 @@ import ICDElectronics
 import time
 
 # Create a new electronics interface object
-electronics = ICDElectronics.Interface("COM11")
+electronics = ICDElectronics.Interface("COM4")
 
 def test():
     print("Well hello world")
 
 electronics.raw.setStartCallback(test)
+
+#time.sleep(10)
 
 '''
 # move upper robot with ID 1 towards the starting area with a speed of 8
@@ -18,3 +20,30 @@ time.sleep(1.4)
 electronics.moveUpperRobot(1, 0)
 print("Robot peatatud ja programm ka lõppes")
 '''
+
+
+
+
+
+# more functions to use:
+
+# moving the upper robot sideways
+# electronics.moveUpperRobot(0, -8) # move upper robot with ID 1 towards the starting area with a speed of 8
+# electronics.moveUpperRobot(0, 0)  # stop the upper robot with ID 1 (sets the speed to 0)
+
+# moving the crane up and down
+# electronics.moveCrane(10)     # moves the crane slowly upwards
+# electronics.moveCrane(-10)    # moves the crane slowly downwards
+# electronics.moveCrane(0)      # stops the crane from moving
+
+# melting the fishing line
+# electronics.meltFishingLine(100)    # tries to melt the fishing line for some time
+# electronics.meltFishingLine(0)      # stops melting (could be used as a safety function if something goes really bad)
+
+# moving the lower robots:
+# electronics.driveRobot(0, 1, -500)   # drives the car with an ID of 0 forward
+# electronics.driveRobot(1, -10)  # drives the car with an ID of 1 backward
+# electronics.driveRobot(0, 0)    # stops the car with an ID of 0
+
+# electronics.rotateRobot(1, 500)    # rotates the robot a few degrees one way
+# electronics.rotateRobot(1, -500)   # rotates the robot a few degrees the other way
